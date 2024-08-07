01e7cdec64031af52206d37f7f17a1ba52a0557c
Rukundo Aime <aime.rukundo@amalitech.com>
2024-08-07 14:50:18 +0200

    feat: add jobseeker-login-integration

--- src/app/jobseekers-login/jobseekers-login.component.ts
+++ src/app/jobseekers-login/jobseekers-login.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-jobseekers-login',
   templateUrl: './jobseekers-login.component.html',
-  styleUrl: './jobseekers-login.component.scss'
+  styleUrl: './jobseekers-login.component.scss',
 })
 export class JobseekersLoginComponent {
-  constructor() {}
+  constructor(private auth: AuthService, private toastr: ToastrService) {}
 
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
@@ -18,6 +20,20 @@ export class JobseekersLoginComponent {
   });
 
   public loginJobseeker() {
-    console.log(this.loginForm.value);
+    this.auth
+      .loginJobSeeker({
+        email: this.loginForm.value.email as string,
+        password: this.loginForm.value.password as string,
+      })
+      .subscribe({
+        next: (jobSeekerData) => {
+          this.toastr.success('successfully logged in');
+          console.log(jobSeekerData);
+        },
+        error: () => {
+          this.toastr.error('something went wrong');
+        },
+      });
+    
   }
 }
