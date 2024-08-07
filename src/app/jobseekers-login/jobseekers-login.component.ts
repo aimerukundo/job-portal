fdaeb33790efe83f76c9cc5f0cd52c7644e57f71
Rukundo Aime <aime.rukundo@amalitech.com>
2024-08-07 16:03:14 +0200

    form controller getters

--- src/app/jobseekers-login/jobseekers-login.component.ts
+++ src/app/jobseekers-login/jobseekers-login.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { REQUIRED_FIELD } from '../../constants/constants';
 
 @Component({
   selector: 'app-jobseekers-login',
   templateUrl: './jobseekers-login.component.html',
-  styleUrl: './jobseekers-login.component.scss'
+  styleUrl: './jobseekers-login.component.scss',
 })
 export class JobseekersLoginComponent {
-  constructor() {}
+  required = '';
+  constructor() {
+    this.required = REQUIRED_FIELD;
+  }
 
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
@@ -16,6 +20,14 @@ export class JobseekersLoginComponent {
       Validators.minLength(8),
     ]),
   });
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
 
   public loginJobseeker() {
     console.log(this.loginForm.value);
